27784573bbec72d4a3cb27a67200ae1dcafc0d5d
JerryGeerts <Jerry8@live.nl>
2019-09-18 15:12:24 +0200

    init

---
+++ UrbanAnalytics/front-end/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from "@angular/router";
+
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {path: "", component: AppComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
