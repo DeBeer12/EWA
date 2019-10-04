c67d54bf8da6b628ca951bc9d09a695f10fce76e
JerryGeerts <Jerry8@live.nl>
2019-10-04 17:41:36 +0200

    Added more mock data for groups

--- UrbanAnalytics/front-end/src/app/services/user/user.service.ts
+++ UrbanAnalytics/front-end/src/app/services/user/user.service.ts
@@ -3,21 +3,46 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
-  userDetails: {
-    firstname: string,
-    lastname: string,
-    email: string
-  } =
+  // TODO: Make model for userData
+  userDetails: { firstname: string, lastname: string, email: string } =
     {
       firstname: "Hans",
       lastname: "Anders",
       email: "HansAnders@gmail.com",
     };
 
+  // TODO: Make model for groupData
+  groupDetails: { groupName: string, createdOn: Date, createdBy: string, memberCount: number, imgUrl: string, id: number, members: [] }[]
+    = [
+      {
+        groupName: "Ewa Project",
+        createdOn: new Date(),
+        createdBy: "Hans Anders",
+        memberCount: 11,
+        imgUrl: "",
+        id: 0,
+        members: []
+      }, {
+        groupName: "EU Energy",
+        createdOn: new Date(),
+        createdBy: "Jerry Geerts",
+        memberCount: 61,
+        imgUrl: "",
+        id: 1,
+        members: [
+
+        ]
+      },
+    ];
   constructor() { }
 
   getUserDetails = () => {
     return this.userDetails;
+  }
+
+  getGroupDetails = () => {
+    return this.groupDetails;
   }
 }
