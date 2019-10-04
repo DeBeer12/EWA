92338f39652afa4fbb6419a95f0ee30e798deaf0
JerryGeerts <Jerry8@live.nl>
2019-10-04 17:42:35 +0200

    Added user provider to main app

--- UrbanAnalytics/front-end/src/app/app.component.ts
+++ UrbanAnalytics/front-end/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UserService]
 })
 export class AppComponent {
   title = 'UrbanAnalytics';
-  navExtended : boolean = false;
+  navExtended: boolean = false;
+
+  constructor(private userService: UserService) { }
 
   changeNav = (event) => {
-   this.navExtended = event;
+    this.navExtended = event;
   }
 }
 
