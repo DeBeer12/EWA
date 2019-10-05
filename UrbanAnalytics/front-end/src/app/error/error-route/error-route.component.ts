c86c6bd70a8a9643602f8fe320391a5570d363fc
Niels <nvoeffel@hotmail.com>
2019-10-05 13:32:09 +0200

    Added 404 page

--- UrbanAnalytics/front-end/src/app/error/error-route/error-route.component.ts
+++ UrbanAnalytics/front-end/src/app/error/error-route/error-route.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-error-route',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error-route.component.scss']
 })
 export class ErrorRouteComponent implements OnInit {
+  pagename: string;
 
-  constructor() { }
-
+  constructor(private router : Router) { }
+  
   ngOnInit() {
+      
+    this.pagename = this.router.url;
   }
 
 }
--- UrbanAnalytics/front-end/src/app/error/error.component.ts
+++ UrbanAnalytics/front-end/src/app/error/error.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
@@ -10,6 +11,7 @@ export class ErrorComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
