2b9dfb2f7f0e173901f24cee494195c060441c89
Danny <danny.huigen@hva.nl>
2019-10-08 13:25:11 +0200

    Making nav/header mobile friendly

--- UrbanAnalytics/front-end/src/app/nav/nav.component.ts
+++ UrbanAnalytics/front-end/src/app/nav/nav.component.ts
@@ -7,7 +7,7 @@ import { NavbarService } from '../services/navbar/navbar.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent{
+export class NavComponent {
 
   navExtended: boolean = true;
   pageChangeObserver = null;
@@ -15,25 +15,25 @@ export class NavComponent{
 
   //All the links in the navbar
   navBarRoutes = [
-    {"text": "Home", "link" : "/dashboard", "iconClasses": "fa fa-3x fa-home medium"},
-    {"text": "Datasets", "link" : "/datasets", "iconClasses": "fa fa-3x fa-bar-chart small"},
-    {"text": "Groups", "link" : "/groups", "iconClasses": "fa fa-3x fa-users small"},
+    { "text": "Home", "link": "/dashboard", "iconClasses": "fa fa-3x fa-home medium" },
+    { "text": "Datasets", "link": "/datasets", "iconClasses": "fa fa-3x fa-bar-chart small" },
+    { "text": "Groups", "link": "/groups", "iconClasses": "fa fa-3x fa-users small" },
   ]
 
   //Constructor
   constructor(
     private router: Router,
-    private navBarService : NavbarService 
-    ) { 
+    private navBarService: NavbarService
+  ) {
     //Subscribing to 'route change' event
     this.pageChangeObserver = router.events.subscribe((val) => {
-        if(val instanceof NavigationEnd){
-          //When the route changed close the navbar
-          this.navBarService.closeNavBar();
-          this.setParentUrl(val);
-        } 
+      if (val instanceof NavigationEnd) {
+        //When the route changed close the navbar
+        this.navBarService.closeNavBar();
+        this.setParentUrl(val);
+      }
     });
-  
+
     //Subscribing to navBar service to see if it's state changes
     this.navBarService.getIsExtended().subscribe(val => {
       //if navbar state is changed set local variable to the one of the service
@@ -42,16 +42,14 @@ export class NavComponent{
   }
 
   //Extracts the parent route from a url
-  setParentUrl(navEnd : NavigationEnd){
+  setParentUrl(navEnd: NavigationEnd) {
     //TODO Get parent url if url is nested with children
     let newUrl = navEnd.url;
     this.currentParentUrl = newUrl;
-    console.log(newUrl);
-    
   }
 
   //Destroys when component unloads from 'page'
-  ngOnDestroy(){
+  ngOnDestroy() {
     //unsubscribing from pageChangeObserver,
     //if not unsubscribing an extra observer will add every route change firering x times for every route page
     this.pageChangeObserver.unsubscribe();
