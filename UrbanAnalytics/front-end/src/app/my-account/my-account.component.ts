8fa17b5e400d8e5fcc1dd6aa9fde9f60d4c93df4
Niels <nvoeffel@hotmail.com>
2019-10-08 19:50:20 +0200

    Profile with edit
    50% done, going to make all the text turn into inputs on next commit

--- UrbanAnalytics/front-end/src/app/my-account/my-account.component.ts
+++ UrbanAnalytics/front-end/src/app/my-account/my-account.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyAccountComponent implements OnInit {
 
-  constructor() { }
+  firstName: string;
+  lastName: string;
+  companyName: string;
+  jobDesc: string;
+  about: string;
+  phoneNum: string;
+  email: string;
+  website: string;
+  loc: string;
+  picUrl: string;
+
+  edit: boolean;
+
+  constructor() {
+    this.edit = true;
+
+  }
 
   ngOnInit() {
+    this.firstName = "Alex";
+    this.lastName = "Ernst";
+    this.companyName = "Sweden Analytics";
+    this.jobDesc = "Junior Data Analist";
+    this.about = "I like to play squash with my mates and beside that I have a big obsession with collection and analysing data about power sources throughout europe Chillwave whatever 90's vexillologist bitters, asymmetrical kale chips enamel pin deserunt lorem pug vape echo park migas.";
+    this.phoneNum = "06-12345678";
+    this.email = "alex.ernst@sweden.se";
+    this.website = "aernst.se";
+    this.loc = "Amsterdam, The Netherlands";
+    this.picUrl = "http://celebsiren.com/wp-content/uploads/2019/03/ALEx-ernst-300x300.jpg";
   }
+
 
 }
