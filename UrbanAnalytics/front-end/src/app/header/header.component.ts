a3b73929cbbb0888b58bc45f3d6658f1a4cd509b
Danny <dannyhuigenzakelijk@gmail.com>
2019-10-15 13:02:49 +0200

    Splitting header SCSS in diffrent files for readability

--- UrbanAnalytics/front-end/src/app/header/header.component.ts
+++ UrbanAnalytics/front-end/src/app/header/header.component.ts
@@ -4,24 +4,28 @@ import { NavbarService } from '../services/navbar/navbar.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss', './hamburger.scss', './notifications.scss']
 })
-export class HeaderComponent{
+export class HeaderComponent {
 
   navExtended: boolean;
+  clicks: number = 0;
 
   constructor(
-    private navBarService : NavbarService
-  ) { 
+    private navBarService: NavbarService
+  ) {
 
-    this.navBarService.getIsExtended().subscribe(val =>{
+    this.navBarService.getIsExtended().subscribe(val => {
       this.navExtended = val;
     });
 
   }
 
+  paasMeDeDisco() {
+    console.log("fayja");
+  }
 
-  toggleNav(){
+  toggleNav() {
     this.navBarService.toggleNavBar();
   }
 }
